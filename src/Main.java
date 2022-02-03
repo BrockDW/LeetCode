99c689e4ea1e2db8c0ba8f2fd0d6d44ddc2e7cb1
Brock <brockdw@outlook.com>
2022-02-03 00:31:01 -0500

    finishing two sum, using hashmap method

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int[] nums = new int[]{3,2,4};
+        ts.twoSum(nums, 6);
+
+    }
+}
---
+++ src/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class TwoSum {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+//            Integer integer = map.get(target - nums[i]);
+            if (map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            };
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
