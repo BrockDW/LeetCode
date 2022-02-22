d715f8466c628f325311c949c5a752ce988492b0
Brock <brockdw@outlook.com>
2022-02-21 20:56:58 -0500

    unwanted folder removed

--- target/generated-test-sources/test-annotations/TwoSum/jmh_generated/TwoSumTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package TwoSum.jmh_generated;
-public class TwoSumTest_jmhType extends TwoSumTest_jmhType_B3 {
-}
-
--- target/generated-test-sources/test-annotations/testingPackage/jmh_generated/JMHTesting_jmhType.java
+++
@@ -1,4 +0,0 @@
-package testingPackage.jmh_generated;
-public class JMHTesting_jmhType extends JMHTesting_jmhType_B3 {
-}
-
